97bd33eaf259d808375dd33305aaa5c33ed188a0
Novie53 <Novie53@users.noreply.github.com>
2019-01-01 21:57:20 +0100

    Update 3min - Debug.py

--- Python/3min - Debug.py
+++ Python/3min - Debug.py
@@ -66,8 +66,8 @@ def Nov_SpeedRun_Two(duration, counter):
 	feature.nuke() #67 = Clock Dimension, #75 = The2DUniverse, #83 = AncientBattlefield
 	time.sleep(1.5)
 	feature.adventure(highest=True)
-	feature.time_machine(19e6, magic=True)
-	feature.augments({"CI": 1}, 27e6)
+	feature.time_machine(17e6, magic=True)
+	feature.augments({"CI": 1}, 28e6)
 	feature.augments({"ML": 1}, 7e6)
 
 	while time.time() < (end - 14): 
@@ -105,7 +105,7 @@ def Nov_SpeedRun_Two(duration, counter):
 				aaa = i.get_bitmap()
 				aaa.save("Pic\\augment1_" + str(counter) + ".png")
 
-				feature.augments({"CI": 1}, 84e6)
+				feature.augments({"CI": 1}, 85e6)
 				feature.augments({"ML": 1}, 11e6)
 				Aug_Assigned = True
 			
