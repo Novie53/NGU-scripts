2c68bbe1dbf9b28ba10162dc97a0036ef04e6193
Novie53 <Novie53@users.noreply.github.com>
2019-01-24 13:58:17 +0100

    Update rebirth.py

--- Python/challenges/rebirth.py
+++ Python/challenges/rebirth.py
@@ -9,7 +9,7 @@ class Rebirth(Features):
 	"""Contains functions for running a no rebirth challenge."""
 
 
-	LOWEST_SLEEP_TO_KILL = 3.7
+	LOWEST_SLEEP_TO_KILL = 3.4
 	ADVENTURE_ZONE = {0: {"name": "Cave of Many Things", "boss": 37, "floor": 4, "sleep": LOWEST_SLEEP_TO_KILL},
 					  1: {"name": "The Sky", "boss": 48, "floor": 5, "sleep": LOWEST_SLEEP_TO_KILL},
 					  2: {"name": "High Security Base", "boss": 58, "floor": 6, "sleep": LOWEST_SLEEP_TO_KILL},
@@ -17,9 +17,11 @@ class Rebirth(Features):
 					  4: {"name": "The 2D Universe", "boss": 74, "floor": 10, "sleep": LOWEST_SLEEP_TO_KILL},
 					  5: {"name": "Ancient Battlefield", "boss": 82, "floor": 11, "sleep": LOWEST_SLEEP_TO_KILL},
 					  6: {"name": "A Very Strange Place", "boss": 90, "floor": 13, "sleep": LOWEST_SLEEP_TO_KILL},
-					  7: {"name": "Mega Lands", "boss": 100, "floor": 14, "sleep": 8},
-					  8: {"name": "The Beardverse", "boss": 108, "floor": 16, "sleep": 9}}
-	MAX_KILL_ADVENTURE_ZONE = 5 #if you only want to kill up towards "Mega Lands" enter 5 and it will avoid Beardverse and onwards
+					  7: {"name": "Mega Lands", "boss": 100, "floor": 14, "sleep": LOWEST_SLEEP_TO_KILL},
+					  8: {"name": "The Beardverse", "boss": 108, "floor": 16, "sleep": LOWEST_SLEEP_TO_KILL},
+					  9: {"name": "Badly Drawn World", "boss": 116, "floor": 18, "sleep": 9},
+					  10: {"name": "Boring-Ass Earth", "boss": 124, "floor": 19, "sleep": 9}}
+	MAX_KILL_ADVENTURE_ZONE = 8 #if you only want to kill up towards "Mega Lands" enter 5 and it will avoid Beardverse and onwards
 
 	def intTryParse(value):
 		try:
@@ -61,11 +63,11 @@ class Rebirth(Features):
 		augment_assigned = 0
 		GoldClearLevels = -1
 		Wandoos_Done = False
-		diggers = [2, 3]
+		diggers = [2, 3, 8]
 
 		self.nuke()
 		time.sleep(1)		
-		self.loadout(1)
+		#self.loadout(1)
 		self.adventure(highest=True)
 
 		while True:
@@ -73,13 +75,23 @@ class Rebirth(Features):
 				self.menu("augmentations")
 				self.click(10, 10)
 				aaa = self.get_bitmap()
-				aaa.save("Pic\\" + str(int(time.time())) + ".png")
+				aaa.save("Pic\\chall7_" + str(int(time.time())) + ".png")
 				return
 
 			self.nuke()
 			self.fight()
 			currentBoss = Rebirth.intTryParse(self.get_current_boss())
 			
+			var1, var2 = self.kill_bosses(currentBoss, 0, GoldClearLevels)
+			if var1:
+				self.adventure(itopod=True, itopodauto=True)
+				GoldClearLevels = var2
+
+				TM_assigned = False
+				augment_assigned = 0
+				#self.menu("digger")
+				#self.click(810, 110)
+			"""
 			if currentBoss > 37:
 				var1, var2 = self.kill_bosses(currentBoss, 0, GoldClearLevels)
 				if var1:
@@ -90,12 +102,12 @@ class Rebirth(Features):
 					augment_assigned -= 1
 					self.menu("digger")
 					self.click(810, 110)
-					diggers = [3]
+			"""
 
 			if currentBoss > 30 and not TM_assigned:
 				self.reclaim_all_energy()
 				self.reclaim_all_magic()
-				self.time_machine(10e6, magic=True)
+				self.time_machine(100e6, magic=True)
 				self.loadout(2)
 				self.augments({"EB": 1}, 80e6)
 				TM_assigned = True
