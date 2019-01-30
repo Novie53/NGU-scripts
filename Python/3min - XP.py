63f3826cddd202ef9f10cbe2bec567b50f0b8e11
Novie53 <Novie53@users.noreply.github.com>
2019-01-30 12:17:20 +0100

    Update 3min - XP.py

--- Python/3min - XP.py
+++ Python/3min - XP.py
@@ -23,7 +23,7 @@ ADVENTURE_ZONE = {0: {"name": "Cave of Many Things", "boss": 37, "floor": 4, "sl
 				  9: {"name": "Badly Drawn World", "boss": 116, "floor": 18, "sleep": LOWEST_SLEEP_TO_KILL},
 				  10: {"name": "Boring-Ass Earth", "boss": 124, "floor": 19, "sleep": 9},
 				  11: {"name": "Chocolate World", "boss": 137, "floor": 21, "sleep": 9}}
-MAX_KILL_ADVENTURE_ZONE = 9 #if you only want to kill up towards "Mega Lands" enter 5 and it will avoid Beardverse and onwards
+MAX_KILL_ADVENTURE_ZONE = 7 #if you only want to kill up towards "Mega Lands" enter 5 and it will avoid Beardverse and onwards
 SCREENSHOT_BOOLEAN = {"aug" : {"Use" : False, "Menu" : "augmentations"},
 					  "TM" : {"Use" : False, "Menu" : "timemachine"},
 					  "blood" : {"Use" : False, "Menu" : "bloodmagic"},
@@ -84,17 +84,18 @@ def Nov_SpeedRun_Two(duration, counter):
 	start = time.time()
 	end = time.time() + (duration * 60)
 
-	feature.nuke(101) #67 = Clock Dimension, #75 = The2DUniverse, #83 = AncientBattlefield
-	currentBoss = 101
-	#time.sleep(1)
+	
+	feature.nuke(50) #83 = AncientBattlefield
 	feature.adventure(highest=True)
-	#time.sleep(1)
 	feature.time_machine(150e6, 100e6)
 	feature.augments({"SM": 1}, 200e6)
 	feature.augments({"AA": 1}, 100e6)
+	
+	feature.nuke(50) #67 = Clock Dimension, #75 = The2DUniverse, #83 = AncientBattlefield
+	currentBoss = intTryParse(feature.get_current_boss())
 
 	while time.time() < (end - 11):
-		if time_since_start() > 130:
+		if time_since_start() > 150:
 			feature.nuke()
 			feature.fight()
 			currentBoss = intTryParse(feature.get_current_boss())
