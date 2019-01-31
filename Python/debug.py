d25b439d9d09deb37e6af9f059b861babb75232f
Novie53 <Novie53@users.noreply.github.com>
2019-01-31 17:19:32 +0100

    Update debug.py

--- Python/debug.py
+++ Python/debug.py
@@ -8,7 +8,6 @@ from classes.inputs import Inputs
 import ngucon as ncon
 
 
-
 w = "derp"
 try:
 	w = Window(debug=True)
@@ -21,7 +20,54 @@ except:
 		print("closing script")
 		quit()	
 i = Inputs()
-Window.x, Window.y = i.pixel_search(ncon.TOP_LEFT_COLOR, 0, 0, 400, 600)
+Window.x, Window.y = i.pixel_search(ncon.TOP_LEFT_COLOR, 10, 10, 400, 600)
+
+
+def get_pixel_color(x, y):
+	def rgb_to_hex(tup):
+		"""Convert RGB value to HEX."""
+		return '%02x%02x%02x'.upper() % (tup[0], tup[1], tup[2])
+
+	"""Get the color of selected pixel in HEX."""
+	dc = win32gui.GetWindowDC(Window.id)
+	rgba = win32gui.GetPixel(dc, x + 8 + Window.x, y + 8 + Window.y)
+	win32gui.ReleaseDC(Window.id, dc)
+	r = rgba & 0xff
+	g = rgba >> 8 & 0xff
+	b = rgba >> 16 & 0xff
+	return rgb_to_hex((r, g, b))
+
+
+	def pixel_search(self, color, x_start, y_start, x_end, y_end):
+		"""Find the first pixel with the supplied color within area.
+
+		Function searches per row, left to right. Returns the coordinates of
+		first match or None, if nothing is found.
+
+		Color must be supplied in hex.
+		"""
+		bmp = self.get_bitmap()
+		width, height = bmp.size
+		for y in range(y_start, y_end):
+			for x in range(x_start, x_end):
+				if y > height or x > width:
+					continue
+				t = bmp.getpixel((x, y))
+				if (self.rgb_to_hex(t) == color):
+					return x - 8, y - 8
+
+		return None
+	
+'''
+for y in range(313 - 1, 329 + 1):
+	for x in range(702 - 1, 751 + 1):
+		pixel = get_pixel_color(x, y)
+		if pixel == "000000":
+			win32api.SetCursorPos((Window.x + x, Window.y + y))
+			print(f"X={x} Y={y} Pixel={pixel}")
+			input("FOUND IT")
+'''
+
 
 '''
 print(w.x, w.y)
